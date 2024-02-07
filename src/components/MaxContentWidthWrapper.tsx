3dcbdaac9436a155d8ba0f67093d4af9efa94435
SergeCodeur <amoussougboserge@gmail.com>
2024-02-07 18:53:47 +0100

    feat: création du composant qui fait office de container

---
+++ src/components/MaxContentWidthWrapper.tsx
@@ -0,0 +1,15 @@
+import { cn } from "../lib/utils";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+const MaxContentWidthWrapper: React.FC<Props> = ({ children, className }) => {
+  return (
+    <div className={cn("max-w-[1050px] max-lg:px-5 mx-auto w-full", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default MaxContentWidthWrapper;
