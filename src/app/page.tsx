f11cad63318d9a74c89c7e80bdb1d24365309f54
SergeCodeur <amoussougboserge@gmail.com>
2024-02-11 18:48:04 +0100

    feat: ajout de section pour la page about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import HeroBanner from "@/components/about/HeroBanner";
+import PartnerSection from "@/components/about/PartnerSection";
 import ProblemStatement from "@/components/about/ProblemStatement";
 import Stats from "@/components/about/Stats";
+import TeamSection from "@/components/about/TeamSection";
+import TestimonialSection from "@/components/about/TestimonialSection";
+import VideoSection from "@/components/about/VideoSection";
 
 export default function About() {
   return (
@@ -8,6 +12,10 @@ export default function About() {
       <HeroBanner />
       <ProblemStatement />
       <Stats />
+      <VideoSection />
+      <TeamSection />
+      <PartnerSection />
+      <TestimonialSection />
     </main>
   );
 }
