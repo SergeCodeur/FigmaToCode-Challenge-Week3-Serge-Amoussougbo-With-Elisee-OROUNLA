86f2d492b83632ce71b0e3a08c28cce63e9bd3e7
SergeCodeur <amoussougboserge@gmail.com>
2024-02-11 18:56:26 +0100

    feat: création de la section team section testimonials

---
+++ src/components/about/TestimonialSection.tsx
@@ -0,0 +1,37 @@
+import TestimonialsImage from "@/assets/images/testimonials-image.png";
+import Image from "next/image";
+import MaxContentWidthWrapper from "../MaxContentWidthWrapper";
+import { Button } from "../ui/button";
+
+const TestimonialSection = () => {
+  return (
+    <div className="bg-hover min-h-[636px]">
+      <MaxContentWidthWrapper className="flex lg:justify-start justify-center items-center min-h-[636px] relative">
+        <div className="max-w-[438px] w-full space-y-6 max-lg:text-center">
+          <h5 className="text-base font-bold text-white uppercase">
+            Work with us
+          </h5>
+          <h2 className="text-5xl text-white max-md:max-w-[274px] max-md:mx-auto">
+            Now Let&apos;s grow Yours
+          </h2>
+          <p className="text-white text-sm max-md:max-w-[257px] max-md:mx-auto tracking-[0.2px]">
+            The gradual accumulation of information about atomic and small-scale
+            behavior during the first quarter of the 20th
+          </p>
+          <Button variant="outline" size="md" className="text-light font-bold">
+            Button
+          </Button>
+        </div>
+        <Image
+          className="absolute -right-[149px] top-0 lg:block hidden"
+          src={TestimonialsImage.src}
+          alt=""
+          width={590}
+          height={636}
+        />
+      </MaxContentWidthWrapper>
+    </div>
+  );
+};
+
+export default TestimonialSection;
