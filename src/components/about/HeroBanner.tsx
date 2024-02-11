eecd5465fa97f1abab91c4912c94d6ed326362ce
SergeCodeur <amoussougboserge@gmail.com>
2024-02-11 18:55:11 +0100

    fix: ajout de couleur

--- src/components/about/HeroBanner.tsx
+++ src/components/about/HeroBanner.tsx
@@ -8,13 +8,14 @@ const HeroBanner = () => {
     <div>
       <MaxContentWidthWrapper className="flex gap-[30px] justify-center items-center max-md:flex-col">
         <div className="lg:w-[599px] py-28 space-y-[35px] w-full max-md:text-center">
-          <h5 className="uppercase text-base font-bold lg:block hidden">
+          <h5 className="uppercase text-text_color text-base font-bold lg:block hidden">
             About company
           </h5>
-          <h1 className="uppercase lg:text-6xl text-5xl">About us</h1>
-          <p className="text-secondary_text_2 text-md">
-            We know how large objects will act, <br /> but things on a small
-            scale
+          <h1 className="uppercase lg:text-6xl text-5xl text-text_color">
+            About us
+          </h1>
+          <p className="text-secondary_text_2 text-md max-w-[376px] max-md:max-w-[274px] max-md:mx-auto">
+            We know how large objects will act, but things on a small scale
           </p>
           <Button variant="primary" size="md" className="h-[52px]">
             Get Quote Now
